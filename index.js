cada763f9d353d5dac1e5a79d7d1c808e641431b
Abhishek Singh <singh4758@gmail.com>
2020-07-25 21:23:13 +0530

    know about app.get(/,function(req,res){ })

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = 8000;
 const app = express();
 
 
+app.get('/',function(req,res){
+    console.log(req);
+    res.end('<h1>Cool It happen</h1>');
+});
+
+
 app.listen(port,function(err){
     if(err){
         console.log(err);
